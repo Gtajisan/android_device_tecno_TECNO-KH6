9b47c5db22b135999a79834b22aa0be69fef83f6
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-09-27 20:25:02 +0600

    No server no recovery

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_TECNO-KH6-user
-add_lunch_combo omni_TECNO-KH6-userdebug
-add_lunch_combo omni_TECNO-KH6-eng
+add_lunch_combo twrp_TECNO-KH6-user
+add_lunch_combo twrp_TECNO-KH6-userdebug
+add_lunch_combo twrp_TECNO-KH6-eng
