7baf31af7ff26a1b35f11b2f26d6575cceea7798
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-09-27 20:24:11 +0600

    Gib money wen

--- twrp_TECNO-KH6.mk
+++ twrp_TECNO-KH6.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from TECNO-KH6 device
 $(call inherit-product, device/tecno/TECNO-KH6/device.mk)
 
 PRODUCT_DEVICE := TECNO-KH6
-PRODUCT_NAME := omni_TECNO-KH6
+PRODUCT_NAME := twrp_TECNO-KH6
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO KH6
 PRODUCT_MANUFACTURER := tecno
